aa42e05feab321af6ffac81f4b4bac26e22fe245
Min Thant Htoo <minthanthtoo.cs@gmail.com>
2025-06-18 12:51:34 +0630

    Updated run.py to runn bot on main thread

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
 # run.py
 
-import threading
+from threading import Thread
 from app import app
-from bot import main as run_bot
+from bot import main as run_bot  # this includes updater.idle()
+
+def start_web():
+    app.run(host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
+    # Start web app in background thread
+    Thread(target=start_web, daemon=True).start()
+
+    # Run Telegram bot in main thread (to allow signals)
+    run_bot()
     app.run(host="0.0.0.0", port=8000)
